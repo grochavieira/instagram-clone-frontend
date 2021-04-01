7343f81675a44c2f499487a0c4062619da4b18c2
Guilherme Rocha <guirocha.hopeisaba@gmail.com>
2021-04-01 19:25:38 -0300

    changed socketio origin URL

--- web/src/contexts/SocketProvider.tsx
+++ web/src/contexts/SocketProvider.tsx
@@ -11,7 +11,8 @@ export const SocketProvider: React.FC<any> = ({ id, children }) => {
   const [socket, setSocket]: any = useState();
 
   useEffect((): any => {
-    const newSocket = io("http://localhost:3333", { query: { id } });
+    const baseURL = process.env.REACT_APP_BASE_URL || "http://localhost:3333";
+    const newSocket = io(baseURL, { query: { id } });
 
     setSocket(newSocket);
     return () => newSocket.close();
